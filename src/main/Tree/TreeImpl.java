f665c8a3fefa3741a79bd8e95670f4737c451de1
geniushkg <geniushkg@gmail.com>
2017-08-26 17:51:54 +0530

    test : tree add and find child also implement same

--- src/main/Tree/TreeImpl.java
+++ src/main/Tree/TreeImpl.java
@@ -2,21 +2,45 @@ package main.Tree;
 
 public class TreeImpl implements Tree {
     private Node root;
+
     public TreeImpl() {
     }
 
     @Override
     public void addChildWithData(int data) {
-        if(root == null){// for empty tree
-            root = new Node(data);
-        }else if (data >= root.getData()){
+        addChildWithDataRecursively(data, root);
+    }
 
+    /**
+     * @param data
+     * @param current
+     */
+    private void addChildWithDataRecursively(int data, Node current) {
+        if (current == null) {
+            current = new Node(data);
+        } else if (data > current.getData()) {
+            addChildWithDataRecursively(data, current.right);
+        } else {
+            addChildWithDataRecursively(data, current.left);
         }
     }
 
     @Override
     public boolean isPresentChildWithData(int data) {
-        return false;
+        return findChildWithDataRecursively(data, root);
+    }
+
+
+    private boolean findChildWithDataRecursively(int data, Node current) {
+        if (current == null) {
+            return false;
+        } else if (data > current.getData()) {
+            return findChildWithDataRecursively(data, current.right);
+        } else if (data < current.getData()) {
+            return findChildWithDataRecursively(data, current.left);
+        } else {
+            return true;
+        }
     }
 
     private class Node {
@@ -30,7 +54,7 @@ public class TreeImpl implements Tree {
             this.data = data;
         }
 
-        public int getData(){
+        public int getData() {
             return data;
         }
     }
--- src/main/Tree/TreeTest.java
+++ src/main/Tree/TreeTest.java
@@ -2,11 +2,24 @@ package main.Tree;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
 public class TreeTest {
 
     @Test
     public void testAddChild() throws Exception {
+        Tree tree = new TreeImpl();
+        tree.addChildWithData(1);
+        assertTrue(tree.isPresentChildWithData(1));
+    }
 
+    @Test
+    public void testFindChild() throws Exception {
+        Tree tree = new TreeImpl();
+        tree.addChildWithData(1);
+        assertFalse(tree.isPresentChildWithData(2));
+        assertTrue(tree.isPresentChildWithData(1));
     }
 
 }
